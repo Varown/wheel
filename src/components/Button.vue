6f25728ddf86da20c8c53047fe4395d31b7b9fb8
PengMeng <383867421@qq.com>
2020-05-10 08:27:42 +0800

    成功引入Button组件，部分css完成

---
+++ src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="g-button">我是按钮</button>
+</template>
+
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Button extends Vue {
+
+  }
+</script>
+
+
+<style lang="scss" scoped>
+
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+  &:hover {
+     border-color: var(--border-color-hover);
+   }
+  &:active {
+     background-color: var(--button-active-bg);
+   }
+  &:focus {
+     outline: none;
+   }
+  }
+</style>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
   </div>
 </template>
+<script>
+  import Vue from "vue";
+  import {Component} from "vue-class-component";
+  import Button from "../components/Button";
+  @Component({
+    components: {Button}
+  })
+  export default class About extends Vue {
+  }
+</script>
