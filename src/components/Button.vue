959777f3fbdbf4e906418258c2a2bdbad4c0910f
PengMeng <383867421@qq.com>
2020-05-18 08:09:08 +0800

    scss

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -27,58 +27,38 @@
   }
 </script>
 
-
 <style lang="scss" scoped>
   @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
   }
-  .g-button {
-    display:inline-flex;
-    justify-content: center;
-    align-items: center;
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+  $border-color-hover: #666;
+  $border-color: #999;
+  $border-radius: 4px;
+  $box-shadow-color: rgba(0, 0, 0, 0.5);
+  $button-active-bg: #eee;
+  $button-bg: white;
+  $button-height: 32px;
+  $color: #333;
+  $font-size: 14px;
+  $input-height: 32px;
+  $red: #F1453D;
+  .g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
+    border-radius: $border-radius; border: 1px solid $border-color;
+    background: $button-bg;
+    display: inline-flex; justify-content: center; align-items: center;
     vertical-align: middle;
-    margin-left: 2px;
-  ;
-  &:hover {
-     border-color: var(--border-color-hover);
-   }
-  &:active {
-     background-color: var(--button-active-bg);
-   }
-  &:focus {
-     outline: none;
-   }
-    > .icon{
-
-      padding-top: .1em;
-      order: 1;
-      margin-right: .1em;
+    &:hover { border-color: $border-color-hover; }
+    &:active { background-color: $button-active-bg; }
+    &:focus { outline: none; }
+    > .g-button-content { order: 2; }
+    > .icon { order: 1; margin-right: .1em; }
+    &.icon-right {
+      > .g-button-content { order: 1; }
+      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
     }
-    > .content{
-      order: 2;
-    }
-    &.icon-right{
-      > .icon{
-        padding-top: .1em;
-        order: 2;
-        margin-right: 0;
-        margin-left: .1em;
-      }
-      > .content{
-        order: 1;
-      }
-    }
-    .loading{
-      padding-top: .1em;
-      animation:spin 2s infinite linear ;
+    .loading {
+      animation: spin 2s infinite linear;
     }
   }
 </style>
-
