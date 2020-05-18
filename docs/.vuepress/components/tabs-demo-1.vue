c4259f97ad3ed597add0101599373fa20d79fa58
PengMeng <383867421@qq.com>
2020-05-18 12:27:26 +0800

    toast 展示

---
+++ docs/.vuepress/components/tabs-demo-1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>简单用法</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <tabs :selected="selected">
+      <tabs-head>
+        <tabs-item name="1">1</tabs-item>
+        <tabs-item name="2">2</tabs-item>
+      </tabs-head>
+      <tabs-body>
+        <tabs-pane name="1">content 1</tabs-pane>
+        <tabs-pane name="2">content 2</tabs-pane>
+      </tabs-body>
+    </tabs>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+  import Tabs from "../../../src/components/Tabs";
+  import TabsBody from "../../../src/components/TabsBody";
+  import TabsHead from "../../../src/components/TabsHead";
+  import TabsItem from "../../../src/components/TabsItem";
+  import TabsPane from "../../../src/components/TabsPane";
+
+  export default {
+    components: {Tabs, TabsBody, TabsHead, TabsItem, TabsPane},
+    data() {
+      return {
+        selected: "1",
+        content: `
+          data:{
+            selected: '1'
+          }
+
+      <tabs :selected="selected">
+      <tabs-head>
+        <tabs-item name="1">1</tabs-item>
+        <tabs-item name="2">2</tabs-item>
+      </tabs-head>
+      <tabs-body>
+        <tabs-pane name="1">content 1</tabs-pane>
+        <tabs-pane name="2">content 2</tabs-pane>
+      </tabs-body>
+    </tabs>
+      `.replace(/^ {8}/gm, "").trim()
+      };
+    }
+  };
+</script>
---
+++ docs/.vuepress/components/toast-demo-1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>this.$toast</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+
+    <div>
+      <Button @click="$toast('点击弹出提示')">上方弹出</Button>
+      <Button @click="$toast('点击弹出提示', {position:'middle'})">中间弹出</Button>
+      <Button @click="$toast('点击弹出提示', {position:'bottom'})">下方弹出</Button>
+    </div>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<style>
+  .wrapper {
+    z-index: 30 !important;
+  }
+</style>
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+
+</style>
+<script>
+  import plugin from '../../../src/plugin/plugin'
+  import Button from '../../../src/components/Button'
+  import Vue from 'vue'
+
+  Vue.use(plugin)
+
+  export default {
+    components: {Button},
+    data () {
+      return {
+        content: `
+          <style>
+            .wrapper {
+              z-index: 30;
+            }
+          </style>
+          <Button @click="$toast('点击弹出提示')">上方弹出</Button>
+          <Button @click="$toast('点击弹出提示', {position:'middle'})">中间弹出</Button>
+          <Button @click="$toast('点击弹出提示', {position:'bottom'})">下方弹出</Button>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
