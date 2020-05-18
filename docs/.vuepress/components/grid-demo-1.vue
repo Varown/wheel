2d4da2a9c3f2cf4508bd43a6e3e8cef3592eca07
PengMeng <383867421@qq.com>
2020-05-18 12:24:46 +0800

    grid  展示

---
+++ docs/.vuepress/components/grid-demo-1.vue
@@ -0,0 +1,214 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>24格网格</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <Row class="demoRow">
+      <List span="8">
+        <div class="demoCol">8</div>
+      </List>
+      <List span="8">
+        <div class="demoCol">8</div>
+      </List>
+      <List span="8">
+        <div class="demoCol">8</div>
+      </List>
+    </Row>
+    <Row class="demoRow">
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+    </Row>
+    <Row class="demoRow">
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+    </Row>
+    <Row class="demoRow">
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+    </Row>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+  .demoRow{
+    margin: 10px 0;
+  }
+  .demoCol {
+    height: 50px;
+    border: 1px solid #ccc;
+    background: #eee;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+<script>
+  import Row from '../../../src/components/Row'
+  import List from '../../../src/components/List'
+
+  export default {
+    components: {Row, List},
+    data () {
+      return {
+        content: `
+          * {
+            box-sizing: border-box;
+          }
+
+          <Row class="demoRow">
+      <List span="8">
+        <div class="demoCol">8</div>
+      </List>
+      <List span="8">
+        <div class="demoCol">8</div>
+      </List>
+      <List span="8">
+        <div class="demoCol">8</div>
+      </List>
+    </Row>
+    <Row class="demoRow">
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+      <List span="6">
+        <div class="demoCol">6</div>
+      </List>
+    </Row>
+    <Row class="demoRow">
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+      <List span="4">
+        <div class="demoCol">4</div>
+      </List>
+    </Row>
+    <Row class="demoRow">
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+      <List span="2">
+        <div class="demoCol">2</div>
+      </List>
+    </Row>
+
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
