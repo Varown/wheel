b86961d49f574676aeb548f0aebd201fcad3a0f0
PengMeng <383867421@qq.com>
2020-05-18 12:26:33 +0800

    input  展示

---
+++ docs/.vuepress/components/input-demo-1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>简单用法</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <Input value="中文"></Input>
+    <Input value="中文" disabled></Input>
+    <Input value="中文" readonly></Input>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+  import Input from '../../../src/components/Input'
+
+  export default {
+    components: {Input},
+    data () {
+      return {
+        selected: '1',
+        content: `
+
+           <Input value="中文"/>
+           <Input value="中文" disabled/>
+           <Input value="中文" readonly/>
+
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
---
+++ docs/.vuepress/components/input-demo-2.vue
@@ -0,0 +1,39 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>双向绑定</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <Input v-model="value"></Input>
+    <div>
+      value: {{value}}
+    </div>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+  import Input from '../../../src/components/Input'
+
+  export default {
+    components: {Input},
+    data () {
+      return {
+        value: '1',
+        content: `
+          data:{
+            value: '1'
+          }
+
+          <Input v-model="value"/>
+          <div>
+            value: {{value}}
+          </div>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
